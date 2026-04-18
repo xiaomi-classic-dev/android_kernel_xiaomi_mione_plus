e679e6203b3c3e2986338d717707d9080209b665
GuaiYiHu <guaiyihu@foxmail.com>
2026-04-18 13:56:41 +0800

    Revert "msm: pm-8x60: Unconditionally support WFI hotplug"
    This reverts commit bda742700a06fa79956726a12266a3c848aa4bf4.

--- arch/arm/mach-msm/pm-8x60.c
+++ arch/arm/mach-msm/pm-8x60.c
@@ -819,7 +819,7 @@ void msm_pm_cpu_enter_lowpower(unsigned int cpu)
 		msm_pm_power_collapse_standalone(false);
 	else if (allow[MSM_PM_SLEEP_MODE_RETENTION])
 		msm_pm_retention();
-	else
+	else if (allow[MSM_PM_SLEEP_MODE_WAIT_FOR_INTERRUPT])
 		msm_pm_swfi();
 }
 
