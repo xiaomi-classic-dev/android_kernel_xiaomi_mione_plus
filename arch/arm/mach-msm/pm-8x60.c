8ee4f2cabaf21bae788c5e1f0115b255f14d8583
GuaiYiHu <guaiyihu@foxmail.com>
2026-04-18 13:56:13 +0800

    Revert "msm: pm-8x60: fix timeout bug issue in suspend processing"
    This reverts commit fc880daa55d92be41f41bc6f3d313bdecc5d770b.

--- arch/arm/mach-msm/pm-8x60.c
+++ arch/arm/mach-msm/pm-8x60.c
@@ -42,7 +42,6 @@
 #include <mach/cpuidle.h>
 #include "idle.h"
 #include "pm.h"
-#include "rpm_resources.h"
 #include "scm-boot.h"
 #include "spm.h"
 #include "timer.h"
@@ -672,11 +671,6 @@ int msm_pm_idle_prepare(struct cpuidle_device *dev,
 		case MSM_PM_SLEEP_MODE_POWER_COLLAPSE_STANDALONE:
 			if (!allow)
 				break;
-
-			if (!dev->cpu && msm_rpm_local_request_is_outstanding()) {
-				allow = false;
-				break;
-			}
 			/* fall through */
 
 		case MSM_PM_SLEEP_MODE_RETENTION:
