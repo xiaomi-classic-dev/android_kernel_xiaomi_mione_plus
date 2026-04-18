e520e277b9174ac8d7f982486b27a58cb05faf2a
GuaiYiHu <guaiyihu@foxmail.com>
2026-04-18 13:53:41 +0800

    Revert "jtagmm: Add save-restore support for Debug and ETM registers"
    This reverts commit ef5b46fbb9f57e1481d46bf2f6e35a9598360a57.

--- arch/arm/mach-msm/include/mach/jtag.h
+++ arch/arm/mach-msm/include/mach/jtag.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012, Code Aurora Forum. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -13,7 +13,7 @@
 #ifndef __MACH_JTAG_H
 #define __MACH_JTAG_H
 
-#if defined(CONFIG_MSM_JTAG) || defined(CONFIG_MSM_JTAG_MM)
+#ifdef CONFIG_MSM_JTAG
 extern void msm_jtag_save_state(void);
 extern void msm_jtag_restore_state(void);
 #else
